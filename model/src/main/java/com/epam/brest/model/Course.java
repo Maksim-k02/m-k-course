d1798d586ea9c790804f4c5c8318ce298ecabb0e
Maksim-k02 <kushnerevich.m@gmail.com>
2022-01-07 15:00:39 +0300

    multimodule structure (model) lesson 14.3

---
+++ model/src/main/java/com/epam/brest/model/Course.java
@@ -0,0 +1,22 @@
+package com.epam.brest.model;
+
+public class Course {
+    private Integer courseId;
+    private String courseName;
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
---
+++ model/src/main/java/com/epam/brest/model/Student.java
@@ -0,0 +1,60 @@
+package com.epam.brest.model;
+
+public class Student {
+    private Integer studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer courseNumber;
+
+    private Integer courseId;
+
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(Integer courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+}
