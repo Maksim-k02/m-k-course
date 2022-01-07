646e0015307e032ccb39ea1adf22412b51c96a75
Maksim-k02 <kushnerevich.m@gmail.com>
2022-01-07 16:04:51 +0300

    dao-api, CoursetDao, CourseDaoJDBCImpl (lesson 15.1)

---
+++ dao-api/src/main/java/com/epam/brest/dao/CourseDao.java
@@ -0,0 +1,12 @@
+package com.epam.brest.dao;
+
+import com.epam.brest.model.Course;
+
+import java.util.List;
+
+public interface CourseDao {
+    List<Course> findAll();
+    Integer create(Course course);
+    Integer update(Course course);
+    Integer delete(Integer courseId);
+}
---
+++ dao/src/main/java/com/epam/brest/dao/CourseDaoJDBCImpl.java
@@ -0,0 +1,27 @@
+package com.epam.brest.dao;
+
+import com.epam.brest.model.Course;
+
+import java.util.List;
+
+public class CourseDaoJDBCImpl implements CourseDao{
+    @Override
+    public List<Course> findAll() {
+        return null;
+    }
+
+    @Override
+    public Integer create(Course course) {
+        return null;
+    }
+
+    @Override
+    public Integer update(Course course) {
+        return null;
+    }
+
+    @Override
+    public Integer delete(Integer courseId) {
+        return null;
+    }
+}
